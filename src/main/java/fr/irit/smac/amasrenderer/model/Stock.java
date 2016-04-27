0e3a025272db5f570ff714a1db57ec203fade4c1
Marjorie Salvetat <marjorie.salvetat@gmail.com>
2016-04-27 11:06:04 +0200

    Refactoring of Stock class + deletion of the sourceDirectory line in the pom

--- src/main/java/fr/irit/smac/amasrenderer/model/Stock.java
+++ src/main/java/fr/irit/smac/amasrenderer/model/Stock.java
@@ -3,16 +3,18 @@ package fr.irit.smac.amasrenderer.model;
 import fr.irit.smac.amasrenderer.Const;
 
 public class Stock {
+
     private String descr;
-    private int rank;
-    
+    private int    rank;
+
     public Stock() {
-        descr = (String)Const.BASE_DESCR[(int)Math.floor(Math.random()*9)];
-        rank = (int)Math.floor(Math.random()*9);
+        descr = Const.BASE_DESCR[(int) Math.floor(Math.random() * 9)];
+        rank = (int) Math.floor(Math.random() * 9);
     }
+
     @Override
     public String toString() {
-        return "\n---Description: "+ descr +
-        "\n---Rank: n°"+rank;
+        return "\n---Description: " + descr +
+            "\n---Rank: n°" + rank;
     }
 }
