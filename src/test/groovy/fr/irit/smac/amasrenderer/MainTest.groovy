fb94b311d8ae383922e8600113a1974ed42aa9f9
Marjorie Salvetat <marjorie.salvetat@gmail.com>
2016-04-27 10:05:06 +0200

    Configuration of Spock

---
+++ src/test/groovy/fr/irit/smac/amasrenderer/MainTest.groovy
@@ -0,0 +1,19 @@
+package fr.irit.smac.amasrenderer
+
+import org.junit.Test
+
+import spock.lang.Specification
+
+class MainTest extends Specification {
+
+    def 'this test is useless'() {
+        
+        when:
+        true
+        
+        then:
+        true
+        println "i'm working !"
+    }
+
+}
