2d28c9ff602ee7cce4d6b0d1d2ac8d83e7f77357
Yononaka0 <yononaka0@gmail.com>
2016-04-28 15:39:49 +0200

    fix to addDelEdge

--- src/main/java/fr/irit/smac/amasrenderer/controller/GraphAddDelEdgeMouseController.java
+++ src/main/java/fr/irit/smac/amasrenderer/controller/GraphAddDelEdgeMouseController.java
@@ -41,34 +41,52 @@ public class GraphAddDelEdgeMouseController extends MouseAdapter {
         graphView.addMouseListener(this);
         buttonAddEdge = buttonDelEdge = false;
     }
-
+    
+    // the headache function
     @Override
     public void mouseClicked(MouseEvent e) {
-        if((source == null) && (e.isShiftDown() || (buttonAddEdge ^ buttonDelEdge))){
-            source = (Node) graphView.findNodeOrSpriteAt(e.getX(), e.getY());
+        //if explanation:
+        //if source = null --> its the first click
+        //and one of the edge button is pressed or shift is down, and control is not down
+        if((source == null) && (e.isShiftDown() || (buttonAddEdge ^ buttonDelEdge)) && (! e.isControlDown())){
+            firstClick(e);
+        }
+        //same but source not null so its the second click
+        else if((source != null) && (e.isShiftDown() || (buttonAddEdge ^ buttonDelEdge)) && (! e.isControlDown())){
+            secondClick(e);
+        }
+        
+    }
+    
+    //this first click sets the source var, and cleans the target var
+    private void firstClick(MouseEvent e){
+        source = (Node) graphView.findNodeOrSpriteAt(e.getX(), e.getY());
+        if(source != null){
             if(!source.hasAttribute("ui.selected")){
                 source.addAttribute("ui.selected");
             }
             target = null;
         }
-        else{
-            target = (Node) graphView.findNodeOrSpriteAt(e.getX(), e.getY());
-            if(target != null){
-                Edge edge = model.getEdge(source.getId()+""+target.getId());
-                if((edge != null) && ((e.isShiftDown() && SwingUtilities.isRightMouseButton(e))^(SwingUtilities.isLeftMouseButton(e) && buttonDelEdge))){
-                    model.removeEdge(edge);
-                }
-                else if((edge == null) && ((SwingUtilities.isLeftMouseButton(e) && (e.isShiftDown() || buttonAddEdge )))){
-                    model.addEdge(source.getId()+""+target.getId(), source.getId(), target.getId(),true);
-                    model.getEdge(source.getId()+""+target.getId()).setAttribute("layout.weight", 20);
-                }
-                source.addAttribute("ui.selected");
-                source.removeAttribute("ui.selected");
-                source = null;
-                
+    }
+    
+    //this second click gets the target var , if it is valid , checks how the used clicked on that node, and tries to add an edge (if addEdgeButton or shift + left click )
+    // or tries to delete it (if user did shift + right click or delEdgeButton + left click )
+    private void secondClick(MouseEvent e){
+        target = (Node) graphView.findNodeOrSpriteAt(e.getX(), e.getY());
+        if(target != null){
+            Edge edge = model.getEdge(source.getId()+""+target.getId());
+            if((edge != null) && (!e.isControlDown()) && ((e.isShiftDown() && SwingUtilities.isRightMouseButton(e))^(SwingUtilities.isLeftMouseButton(e) && buttonDelEdge))){
+                model.removeEdge(edge);
             }
+            else if((edge == null) && ((SwingUtilities.isLeftMouseButton(e) && (e.isShiftDown() || buttonAddEdge )))){
+                model.addEdge(source.getId()+""+target.getId(), source.getId(), target.getId(),true);
+                model.getEdge(source.getId()+""+target.getId()).setAttribute("layout.weight", 20);
+            }
+            source.addAttribute("ui.selected");
+            source.removeAttribute("ui.selected");
+            source = null;
+            
         }
-        
     }
     
         
