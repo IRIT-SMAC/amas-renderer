79c8f4bfd16b9d3887c023227f12967e431b87f1
GaelMarin <mrn.gael@gmail.com>
2016-04-28 14:43:38 +0200

    Mise à niveau
    Mise à niveau pour construire la nouvelle interface

--- src/main/java/fr/irit/smac/amasrenderer/Main.java
+++ src/main/java/fr/irit/smac/amasrenderer/Main.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -20,6 +21,7 @@ public class Main extends Application {
         this.initRootLayout();
         this.initGraphAgents();
         this.initServices();
+        this.initOptions();
 
         primaryStage.setScene(new Scene(rootLayout));
         primaryStage.show();
@@ -30,6 +32,13 @@ public class Main extends Application {
         FXMLLoader loaderRootLayout = new FXMLLoader();
         loaderRootLayout.setLocation(Main.class.getResource("view/RootLayout.fxml"));
         this.rootLayout = (BorderPane) loaderRootLayout.load();
+    }
+    
+    private void initOptions() throws IOException {
+    	FXMLLoader loaderGraphAgents = new FXMLLoader();
+        loaderGraphAgents.setLocation(Main.class.getResource("view/optionsLayout.fxml"));
+        HBox root = (HBox) loaderGraphAgents.load();  
+        rootLayout.setBottom(root);
     }
     
     private void initGraphAgents() throws IOException {
--- src/test/groovy/fr/irit/smac/amasrenderer/IHMTest.groovy
+++ src/test/groovy/fr/irit/smac/amasrenderer/IHMTest.groovy
@@ -1,51 +1,69 @@
 package fr.irit.smac.amasrenderer
 
 import static org.testfx.api.FxAssert.verifyThat
-import static org.testfx.matcher.base.NodeMatchers.hasText
-import javafx.event.ActionEvent
-import javafx.event.EventHandler
-import javafx.scene.control.Button
-import javafx.scene.control.Label
-import javafx.scene.control.TextField
-import javafx.scene.layout.HBox
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.BorderPane
+import fr.irit.smac.amasrenderer.controller.GraphMainController
 
 class IHMTest extends GuiSpecification{
 
-    /** This is an example of using TestFX **/
-    
-    def setup() {
-        setupStage { stage ->
-            def passwordField = new TextField(id: "password")
-            def submitButton = new Button(id: "submit", text: "submit")
-            def messageLabel = new Label(id: "message")
+	/** This is an example of using TestFX **/
 
-            submitButton.onAction = { ActionEvent event ->
-                if (passwordField.text == "sheogorath") {
-                    messageLabel.text = "please enter!"
-                }
-                else {
-                    messageLabel.text = "wrong password!"
-                }
-            } as EventHandler
-            return new HBox(passwordField, submitButton, messageLabel)
-        }
-    }
+	/*def setup() {
+	 setupStage { stage ->
+	 def passwordField = new TextField(id: "password")
+	 def submitButton = new Button(id: "submit", text: "submit")
+	 def messageLabel = new Label(id: "message")
+	 submitButton.onAction = { ActionEvent event ->
+	 if (passwordField.text == "sheogorath") {
+	 messageLabel.text = "please enter!"
+	 }
+	 else {
+	 messageLabel.text = "wrong password!"
+	 }
+	 } as EventHandler
+	 return new HBox(passwordField, submitButton, messageLabel)
+	 }
+	 }*/
 
-    def "enters gate with right password"() {
-        when:
-        fx.clickOn("#password").write("sheogorath")
-        fx.clickOn("#submit")
+	BorderPane rootLayout;
 
-        then:
-        verifyThat("#message", hasText("please enter!"))
-    }
+	private void initRootLayout() throws IOException {
 
-    def "enters gate with wrong password"() {
-        when:
-        fx.clickOn("#password").write("asura")
-        fx.clickOn("#submit")
+		FXMLLoader loaderRootLayout = new FXMLLoader();
+		loaderRootLayout.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+		this.rootLayout = (BorderPane) loaderRootLayout.load();
+	}
 
-        then:
-        verifyThat("#message", hasText("wrong password!"))
-    }
+	private void initGraphAgents() throws IOException {
+
+		FXMLLoader loaderGraphAgents = new FXMLLoader();
+		loaderGraphAgents.setLocation(Main.class.getResource("view/GraphAgents.fxml"));
+		AnchorPane root = (AnchorPane) loaderGraphAgents.load();
+		GraphMainController controller = loaderGraphAgents.getController();
+		controller.drawGraph();
+		rootLayout.setCenter(root);
+	}
+
+
+	def setup() {
+		setupStage { stage ->
+			this.initRootLayout();
+			this.initGraphAgents();
+
+			return new Scene(rootLayout);
+		}
+	}
+
+	def "Premier test" () {
+		given:
+		println "alakon"
+		when:
+		println "lol"
+		fx.clickOn("#attributes_synthesis")
+		then:
+		true
+	}
 }
