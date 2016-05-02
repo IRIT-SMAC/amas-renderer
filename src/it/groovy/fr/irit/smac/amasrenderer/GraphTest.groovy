fec7abb1fb480217b1e885b8699df321c2a239d3
GaelMarin <mrn.gael@gmail.com>
2016-05-02 09:45:27 +0200

    First test

--- src/it/groovy/fr/irit/smac/amasrenderer/GraphTest.groovy
+++ src/it/groovy/fr/irit/smac/amasrenderer/GraphTest.groovy
@@ -1,74 +1,84 @@
 package fr.irit.smac.amasrenderer
 
 
-import java.io.IOException
-
-import fr.irit.smac.amasrenderer.controller.GraphMainController
 import javafx.embed.swing.SwingNode
 import javafx.fxml.FXMLLoader
 import javafx.scene.input.KeyCode
-import javafx.scene.layout.AnchorPane
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.StackPane
+import spock.lang.Shared
+import fr.irit.smac.amasrenderer.controller.GraphMainController
+import fr.irit.smac.amasrenderer.service.GraphService
 
 class GraphTest extends GuiSpecification{
 
+	@Shared
+	GraphService graphService
+	
     def setup() {
         setupStage { stage ->
 
             BorderPane rootLayout = initRootLayout()
-            AnchorPane root = initGraphAgents()
+            BorderPane root = initGraphAgents()
             rootLayout.setCenter(root)
             return rootLayout
         }
 
         sleep(1000) //time for the graph to be initialized
+		graphService = GraphService.getInstance()
     }
 
     private BorderPane initRootLayout() throws IOException {
-
-        FXMLLoader loaderRootLayout = new FXMLLoader()
-        loaderRootLayout.setLocation(Main.class.getResource("view/RootLayout.fxml"))
-        return (BorderPane) loaderRootLayout.load()
+        
+        FXMLLoader loaderRootLayout = new FXMLLoader();
+        loaderRootLayout.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+        return (BorderPane) loaderRootLayout.load();
     }
-
-    private AnchorPane initGraphAgents() throws IOException {
-
-        FXMLLoader loaderGraphAgents = new FXMLLoader()
-        loaderGraphAgents.setLocation(Main.class.getResource("view/GraphAgents.fxml"))
-        AnchorPane root = (AnchorPane) loaderGraphAgents.load()
-        SwingNode swingNode = new SwingNode()
-        swingNode.setId("graphNode")
-        StackPane stackPaneGraphNode = (StackPane) root.lookup("#stackPaneGraphNode")
-        stackPaneGraphNode.getChildren().add(swingNode)
-        GraphMainController controller = loaderGraphAgents.getController()
-        controller.drawGraph()
-        return root
+    
+    
+    private BorderPane initGraphAgents() throws IOException {
+        
+        FXMLLoader loaderGraphAgents = new FXMLLoader();
+        loaderGraphAgents.setLocation(Main.class.getResource("view/GraphAgents.fxml"));
+        BorderPane root = (BorderPane) loaderGraphAgents.load();
+        SwingNode swingNode = new SwingNode();
+        swingNode.setId("graphNode");
+        StackPane stackPaneGraphNode = (StackPane) root.lookup("#stackPaneGraphNode");
+        stackPaneGraphNode.getChildren().add(swingNode);
+        GraphMainController controller = loaderGraphAgents.getController();
+        controller.drawGraph();
+		return root;
     }
 
     def "check if an agent is added by clicking on the corresponding button"() {
 
+		
+		given:
+		def nbNoeud = graphService.getModel().getNodeCount()
+		
         when:
-        fx.clickOn("#addAgent")
+        fx.clickOn("#buttonAddAgent").moveBy(0,-100).clickOn(MouseButton.PRIMARY)
 
         then:
-        false
-        //Verifier dans le modele qu'un agent a ete ajoute
+        graphService.getModel().getNodeCount() == (nbNoeud+1)
     }
 
     def "check if an agent is added by doing the corresponding shortcut"() {
 
+		given:
+		def nbNoeud = graphService.getModel().getNodeCount()
+		
         when:
         println "addition of an agent - shortcut"
-        //changer ALT par SHIFT
-        fx.press(KeyCode.ALT).clickOn("#graphNode").release(KeyCode.ALT)
+        fx.moveBy(-200,-200).press(KeyCode.CONTROL).clickOn("#graphNode").release(KeyCode.CONTROL)
 
         then:
-        false
-        //Verifier dans le modele qu'un agent a ete ajoute
+		graphService.getModel().getNodeCount() == (nbNoeud+1)
+		
     }
 
-    def "check if an agent is removed by clicking on the corresponding button"() {
+    /*def "check if an agent is removed by clicking on the corresponding button"() {
 
         when:
         //fx.clickOn("#deleteAgent")
@@ -127,5 +137,5 @@ class GraphTest extends GuiSpecification{
         then:
         false
         //Verifier dans le modele qu'un agent a ete ajoute
-    }
+    }*/
 }
