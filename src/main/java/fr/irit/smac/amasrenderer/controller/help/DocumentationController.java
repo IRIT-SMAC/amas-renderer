6a5bd9c0e5c6422c433065aedda3b80c04d26517
Marjorie Salvetat <marjorie.salvetat@gmail.com>
2016-06-11 19:39:43 +0200

    Use of a html file to display the documentation : more efficient compare to javafx

---
+++ src/main/java/fr/irit/smac/amasrenderer/controller/help/DocumentationController.java
@@ -0,0 +1,40 @@
+package fr.irit.smac.amasrenderer.controller.help;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker.State;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class DocumentationController implements Initializable{
+
+    @FXML
+    WebView doc;
+    
+    @FXML
+    ProgressBar progressBar;
+
+    
+    public void initialize(URL location, ResourceBundle resources) {
+        WebEngine engine = doc.getEngine();
+        engine.load(DocumentationController.class.getResource("../../html/documentation.html").toExternalForm()); 
+        
+        progressBar.progressProperty().bind(engine.getLoadWorker().progressProperty());
+
+        engine.getLoadWorker().stateProperty().addListener(
+                new ChangeListener<State>() {
+                    @Override
+                    public void changed(ObservableValue ov, State oldState, State newState) {
+                        if (newState == State.SUCCEEDED) {
+                            progressBar.setVisible(false);
+                        }
+                    }
+                });
+    }
+}
